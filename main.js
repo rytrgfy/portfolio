7a8ba07d5b7140061ccbb3fee6cd74021c637cca
Rohit Kumar Sahoo <rohitkumarsahoo2002@gmail.com>
2024-03-05 20:22:03 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,9 @@
+alert("please try to take your cursur in images");
+
+var typing = new Typed(".typing", {
+
+    strings: ["FrontEnd Developer", "Web Designer", "Freelancer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
